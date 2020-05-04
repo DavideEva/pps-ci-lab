dae0514428826fcac4114310d12ff019c5366c15
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-05-04 15:11:30 +0200

    Add @params

--- src/main/java/Delu.java
+++ src/main/java/Delu.java
@@ -6,6 +6,7 @@ public class Delu {
     /**
      * Javadoc is my passion!
      * This is the main.
+     * @param args command line arguments
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
