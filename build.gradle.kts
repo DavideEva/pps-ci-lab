720e105cee7f9e53f5eac83ebf1a13d04623bbe6
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-05-04 16:45:00 +0200

    Add tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,20 @@
 plugins {
     java
     scala
+    id("com.github.maiflai.scalatest") version "0.26"
 }
 
 repositories {
     mavenCentral()
 }
 
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
 dependencies {
     implementation("org.scala-lang:scala-library:2.13.2")
     testImplementation("org.scalatest:scalatest_2.13:3.1.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+    testRuntimeOnly("com.vladsch.flexmark:flexmark-profile-pegdown:0.36.8")
 }
---
+++ src/test/scala/DeluTest.scala
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions._
+
+class DeluTest {
+
+    @Test
+    def mainTest(): Unit = {
+        ScalaDelu.main(Array("Hello"))
+        assertTrue(false)
+    }
+}
