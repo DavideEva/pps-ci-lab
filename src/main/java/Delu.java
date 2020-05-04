fe0eb9aba173cda96462429dbe2853b812347633
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-05-04 14:56:41 +0200

    add javadoc

--- src/main/java/Delu.java
+++ src/main/java/Delu.java
@@ -1,4 +1,12 @@
+/**
+ * A super useful class.
+*/
 public class Delu {
+
+    /**
+     * Javadoc is my passion!
+     * This is the main.
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
